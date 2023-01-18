39e2fc3b2eb1fa66fdebd65b7e23ec48546dfa28
Petri <peewter@gmail.com>
2023-01-18 20:56:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import Tracker from './Tracker';
+import Info from './Info';
+import Tasks from './Tasks';
+import Categories from './Categories';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className='App'>
+          <nav>
+            <div className='links'>
+              <Link to='/'>Seuranta</Link>
+              <Link to='/tasks'>Tehtävät</Link>
+              <Link to='/categories'>Kategoriat</Link>
+              <Link to='/info'>Tietoa</Link>
+            </div>
+          </nav>
+
+          <Routes>
+            <Route path='/' element={<Tracker />} />
+            <Route path='/tasks/*' element={<Tasks />} />
+            <Route path='/categories/*' element={<Categories />} />
+            <Route path='info/*' element={<Info />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
+// npx json-server -H localhost -p 3010 -w ./db.json

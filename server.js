f8b345795e8fb527a794d620b5df8da82dfb9567
Petri <peewter@gmail.com>
2023-01-31 13:52:42 +0200

    Install cors module and add to server

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 3010;
+const cors = require('cors');
+
 
 server.use(middlewares);
 server.use(router);
+server.use(cors());
 
 server.listen(PORT, () => {
   console.log(`Backend is listening on port ${PORT}`);
